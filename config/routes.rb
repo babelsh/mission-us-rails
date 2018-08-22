7d27e6f803e7e22bf1c3bffd4b7628d3114db660
Carlos Eduardo Cuzik <cuzikjack@gmail.com>
2018-08-22 15:59:40 -0300

    add dashboard (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  # get 'dashboard/index'
-
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root controller: :dashboard, action: :index
+  resources :dashboard, only: :index
+
+  root 'dashboard#index'
 end
