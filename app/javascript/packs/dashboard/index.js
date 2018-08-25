4032d4b4865b5881c0ac08831af5fd0d0dbd51a0
rartner <ramon.artner@gmail.com>
2018-08-25 19:58:25 -0300

    configuring vuetify

---
+++ app/javascript/packs/dashboard/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './components/app.vue'
+import Vuetify from "vuetify";
+
+import colors from "vuetify/es5/util/colors";
+
+Vue.use(Vuetify);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = document.querySelector("#dashboard");
+  const app = new Vue({ 
+    el,
+    render: h => h(App),
+  });
+})
