e697f937027cfbf178336523edede851e3e5fe65
Ramon Artner <ramon.artner@gmail.com>
2018-08-26 01:24:49 -0300

    create dashboard components (#19)
    * create dashboard components

---
+++ app/javascript/packs/components/Trail.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-card>
+      <v-card-media 
+        class="white--text" 
+        height="200px" 
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-container fill-height fluid>
+          <v-layout fill-height>
+            <v-flex xs12 align-end flexbox>
+              <span class="headline">{{ title }}</span>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-media>
+      <v-card-actions>
+        <v-btn icon>
+          <v-icon>favorite</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>bookmark</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>face</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+
+export default {
+  name: 'trail',
+  props: {
+    title: String,
+  },
+};
+
+</script>
---
+++ app/javascript/packs/dashboard/components/Dashboard.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-app>
+    <v-container style="max-width: 100%">
+      <v-carousel style="height: 250px">
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    </v-container>
+    <v-container fluid grid-list-md>
+      <v-layout row wrap align-center>
+        <v-flex xs12 md6 lg6>
+          <v-date-picker 
+            v-model="date"
+            locale="pt-br"
+            no-title
+            reactive
+            full-width
+            class="mt-3"
+          ></v-date-picker>
+        </v-flex>
+        <v-flex class="text-xs-center">
+          <div>
+            <v-progress-circular
+              :rotate="360"
+              :size="100"
+              :width="15"
+              :value="progress"
+              color="teal"
+            >
+              {{ progress }}%
+            </v-progress-circular>
+          </div>
+          <div>
+            Conclusão das atividades da semana
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs6 md4 lg4 v-for="trail in trails" :key="trail.name">
+          <trail :title="trail.name" />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import Trail from '../../components/Trail';
+
+export default {
+  name: 'dashboard',
+  components: {
+    Trail,
+  },
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+        }
+      ],
+      trails: [
+        {
+          name: 'Trilha 1',
+        },
+        {
+          name: 'Trilha 2',
+        },
+        {
+          name: 'Trilha 3',
+        },
+        {
+          name: 'Trilha 4',
+        },
+      ],
+      date: '2018-08-20',
+      progress: 37,
+    };
+  },
+};
+</script>
