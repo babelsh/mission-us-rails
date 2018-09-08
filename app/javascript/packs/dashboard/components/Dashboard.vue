55b87592851628e0c39f3316eed69ee185297d69
Ramon Artner <ramon.artner@gmail.com>
2018-09-07 21:39:17 -0300

    fixing some dashboard components (#27)
    * fixing dashboard components
    
    * adding card image source as a prop in the trail component

--- app/javascript/packs/dashboard/components/Dashboard.vue
+++ app/javascript/packs/dashboard/components/Dashboard.vue
@@ -1,13 +1,12 @@
 <template>
   <v-app>
-    <v-container style="max-width: 100%">
-      <v-carousel style="height: 250px">
-        <v-carousel-item
-          v-for="(item,i) in items"
-          :key="i"
-          :src="item.src"
-        ></v-carousel-item>
-      </v-carousel>
+    <v-container fluid>
+      <trail
+        isActive
+        isFavorite
+        title="Trilha de introdução"
+        :source="source"
+      ></trail>
     </v-container>
     <v-container fluid grid-list-md>
       <v-layout row wrap align-center>
@@ -33,7 +32,7 @@
               {{ progress }}%
             </v-progress-circular>
           </div>
-          <div>
+          <div class="text-current-activity">
             Conclusão das atividades da semana
           </div>
         </v-flex>
@@ -42,7 +41,7 @@
     <v-container fluid grid-list-md>
       <v-layout row wrap>
         <v-flex xs6 md4 lg4 v-for="trail in trails" :key="trail.name">
-          <trail :title="trail.name" />
+          <trail :title="trail.name" :source="source"/>
         </v-flex>
       </v-layout>
     </v-container>
@@ -88,8 +87,16 @@ export default {
         },
       ],
       date: '2018-08-20',
-      progress: 37,
+      progress: 67,
+      source: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
     };
   },
 };
 </script>
+
+<style>
+.text-current-activity {
+  padding-top: 10px;
+  font-size: 14px;
+}
+</style>
